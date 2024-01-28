251f894ed9f5dac4dee9c3ba76c25202b5bd299e
g ws <gengwenshuai1@yeah.net>
2024-01-28 21:14:12 +0800

    add isLeapYear

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -3,6 +3,12 @@
  */
 public class LeapYear {
 
+    /** determin if the given year is a leap year
+    *   @param year to be analyzed
+    */
+    public static boolean isLeapYear(int year){
+        return (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0);
+    }
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
      */
