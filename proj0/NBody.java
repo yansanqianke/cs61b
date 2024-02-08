99192b73c5d86df9ff87f9546e31bf1286d45396
g ws <gengwenshuai1@yeah.net>
2024-02-08 18:13:35 +0800

    add read planets func

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -4,4 +4,16 @@ public class NBody {
         in.readDouble();
         return in.readDouble();
     }
+
+    public static Planet[] readPlanets(String str){
+        In in=new In(str);
+        int num=in.readInt();
+        Planet[] ap=new Planet[num];
+        in.readDouble();
+        for(int i=0;i<num;i++){
+            ap[i]=new Planet(in.readDouble(),in.readDouble(),in.readDouble(),
+            in.readDouble(),in.readDouble(),in.readString());
+        }
+        return ap;
+    }
 }
